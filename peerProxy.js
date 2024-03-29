e17f6493bf6a4e0bfdc351553a17f17a96b2a618
Ryann Penrod <ryannpenrod@gmail.com>
2024-03-29 15:23:55 -0600

    deleted some not working stuff

--- peerProxy.js
+++ peerProxy.js
@@ -2,40 +2,6 @@
 
 
 
-
-
-
-
-// // peerProxy.js
-
-// const WebSocket = require('ws');
-
-// const wss = new WebSocket.Server({ port: 3000 });
-
-// // Event listener for WebSocket connections
-// wss.on('connection', function connection(ws) {
-//     console.log('WebSocket connection established');
-
-//     // Event listener for receiving messages from clients
-//     ws.on('message', function incoming(message) {
-//         console.log('Received:', message);
-        
-//         // Broadcast the message to all connected clients
-//         wss.clients.forEach(function each(client) {
-//             if (client !== ws && client.readyState === WebSocket.OPEN) {
-//                 client.send(message);
-//             }
-//         });
-//     });
-
-//     // Event listener for WebSocket disconnection
-//     ws.on('close', function close() {
-//         console.log('WebSocket connection closed');
-//     });
-// });
-
-
-
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3000 });
