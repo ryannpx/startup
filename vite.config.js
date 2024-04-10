bd598205104017b46e27b3f282af1ba35e046592
Ryann Penrod <ryannpenrod@gmail.com>
2024-04-10 15:02:52 -0600

    bundled with vite/login auth works

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    server: {
+        proxy: {
+            '/api': 'http://localhost:4000',
+            '/auth': 'http://localhost:4000',
+            '/user': 'http://localhost:4000',
+            '/ws' : {
+                target: 'ws://localhost:4000',
+                ws: true,
+            },
+        },
+    },
+});
